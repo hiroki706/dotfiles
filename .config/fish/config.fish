0885acbbe1e5dd093a96c6e016ea97224339432e
s1f10230101 <s1f102301011@iniad.org>
2026-02-01 15:25:23 +0900

    a

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,11 @@ set fish_greeting
 export FIREFOX_BIN="/mnt/c/Program Files/Mozilla Firefox/firefox.exe"
 abbr -a lg lazygit
 alias mr='mise r'
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
--- .config/nushell/config.nu
+++ .config/nushell/config.nu
@@ -28,4 +28,12 @@ $env.config.show_banner = false
 # ユーティリティ用エイリアス
 alias lg = lazygit
 alias mr = mise r
-
+def --env y [...args] {
+	let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+	^yazi ...$args --cwd-file $tmp
+	let cwd = (open $tmp)
+	if $cwd != $env.PWD and ($cwd | path exists) {
+		cd $cwd
+	}
+	rm -fp $tmp
+}
